6f04262f728fc6d743d62119ff8093dbfa08e274
Matheus Viana <matheus_o_viana@hotmail.com>
2023-02-25 18:31:10 -0300

    add a StateObject and State to track showing a view to add new activities to display a list of activities

--- HabitTracking/ContentView.swift
+++ HabitTracking/ContentView.swift
@@ -8,6 +8,9 @@
 import SwiftUI
 
 struct ContentView: View {
+    @StateObject var activities = Activities()
+    @State private var showingAddHabit = false
+    
     var body: some View {
         VStack {
             Image(systemName: "globe")
