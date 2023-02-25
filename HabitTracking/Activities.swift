eba7dc6e53c102a1700daa8b53e448afd03b21d3
Matheus Viana <matheus_o_viana@hotmail.com>
2023-02-25 18:36:39 -0300

    Encoding and decoding saved data

--- HabitTracking/Activities.swift
+++ HabitTracking/Activities.swift
@@ -8,5 +8,22 @@
 import Foundation
 
 class Activities: ObservableObject {
-    @Published var activities: [ActivityItem] = []
+    @Published var activities: [ActivityItem] = [] {
+        didSet {
+            let enconder = JSONEncoder()
+            
+            if let encoded = try? enconder.encode(activities) {
+                UserDefaults.standard.set(encoded, forKey: "activities")
+            }
+        }
+    }
+    
+    init() {
+        if let savedActivities = UserDefaults.standard.data(forKey: "activities") {
+            if let decodedActivities = try? JSONDecoder().decode([ActivityItem].self, from: savedActivities) {
+                activities = decodedActivities
+                return
+            }
+        }
+    }
 }
