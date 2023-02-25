c0f08bb7500fcafd3de9a0938caa627d56e64e45
Matheus Viana <matheus_o_viana@hotmail.com>
2023-02-24 22:48:52 -0300

    Initial Commit

---
+++ HabitTracking/HabitTrackingApp.swift
@@ -0,0 +1,17 @@
+//
+//  HabitTrackingApp.swift
+//  HabitTracking
+//
+//  Created by Matheus Viana on 24/02/23.
+//
+
+import SwiftUI
+
+@main
+struct HabitTrackingApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
